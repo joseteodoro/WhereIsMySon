6bdd9e865d9341abb0458432534dd531ad3f6127
joseteodoro <jteodoro.dev@gmail.com>
2018-06-24 15:50:32 -0300

    adding comments about the mock on anomaly detector

--- notification-service/src/main/java/br/whereismyson/notificationservice/anomalyDetection/AnomalyDetector.java
+++ notification-service/src/main/java/br/whereismyson/notificationservice/anomalyDetection/AnomalyDetector.java
@@ -7,6 +7,8 @@ import br.whereismyson.notificationservice.model.GeoPosition;
 @Component
 public class AnomalyDetector {
 
+	// mock one, should be replaced with some machine learning code to use the geoposition to load the 
+	// device usual positions and understand if there is a anomaly or not.
 	public Boolean hasAnomaly(GeoPosition geoPosition) {
 		return false;
 	}
