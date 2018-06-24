feb86c36b9f3d5fe1abfcfb6c0ea1a8c6af30fb8
joseteodoro <jteodoro.dev@gmail.com>
2018-06-24 11:37:37 -0300

    adding springboot start application

---
+++ localization-service/src/main/java/br/whereismyson/localizationservice/LocalizationServiceApplication.java
@@ -0,0 +1,12 @@
+package br.whereismyson.localizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizationServiceApplication.class, args);
+	}
+}
